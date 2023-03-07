82c5c908d77fcbfd90873be966125c520722a495
DhaosCollider <72247447+DhaosCollider@users.noreply.github.com>
2023-03-07 15:03:38 +0900

    Update Register.lua

--- Register.lua
+++ Register.lua
@@ -19,11 +19,11 @@ local function activateTableSettings(author)
         local msg = state and "Available" or "Unavailable"
         Obj.log(msg..": "..al.description)
         Obj.log('-------------------------------------------------------------------------------')
-        synchronize(function() al.color = state and Obj.enabledColor or Obj.disabledColor end)
+        al.color = state and Obj.enabledColor or Obj.disabledColor
         ::continue::
     end
 end
----------------------------------------------------------------------------------------------------
+
 function Obj.onEnable(...)
     Obj.log('-------------------------------------------------------------------------------')
     local author = {...}
@@ -36,12 +36,12 @@ local function deactivateTableSettings(author)
     for key, tbl in pairs(author) do
         local al = AddressList[key]
         if not al then goto continue end
-        synchronize(function() if al then al.color = Obj.disabledColor end end)
+        if al then al.color = Obj.disabledColor end
         for i = 1, #tbl do local arg = tbl[i]; unregisterSymbol(arg[1]) end
         ::continue::
     end
 end
----------------------------------------------------------------------------------------------------
+
 function Obj.onDisable(...)
     local author = {...}
     for i = 1, #author do deactivateTableSettings(author[i]) end
@@ -57,26 +57,26 @@ local function memScan(strAOB, start, size)
     memscan_waitTillDone(ms); foundlist_initialize(fl)
     local fc = foundlist_getCount(fl)
     if (fc < 1) then ms.destroy(); fl.destroy(); return end
-    local Result = createStringlist()
-    for i = 0, fc - 1 do Result.add(fl.getAddress(i)) end
+    local Result = {}
+    for i = 0, fc - 1 do Result[#Result + 1] = fl.getAddress(i) end
     ms.destroy(); fl.destroy(); return Result
 end
----------------------------------------------------------------------------------------------------
+
 local function customScan(strSymbol, strAOB, selectNum, start, size)
     local Result = memScan(strAOB, start, size)
     if not Result then Obj.log("Error: "..strSymbol.." AOB not found"); return end
     local msg = "Error: "..strSymbol.." AOB Result.count < selectNum"
-    if (Result.count < selectNum) then Result.destroy(); Obj.log(msg); return end
-    local address = Result[selectNum - 1]
-    Result.destroy(); return address
+    if (#Result < selectNum) then Obj.log(msg) return end
+    local address = Result[selectNum]
+    return address
 end
----------------------------------------------------------------------------------------------------
+
 local function aobScanRegion(SymbolName, StartAddress, StopAddress, AOBString)
     local str = 'aobScanRegion(%s, %X, %X, %s)\nregistersymbol(%s)'
     local script = str:format(SymbolName, StartAddress, StopAddress, AOBString, SymbolName)
     return autoAssemble(script)
 end
----------------------------------------------------------------------------------------------------
+
 function Obj.multiCustomScan(args)
     local isAvailable = true
     for i = 1, #args do
@@ -92,13 +92,24 @@ function Obj.multiCustomScan(args)
     return isAvailable
 end
 ---------------------------------------------------------------------------------------------------
+-- enableMemoryRecords functions
+---------------------------------------------------------------------------------------------------
+local function enableMemoryRecordVariable(variableTable)
+    if not variableTable[1].value then return end
+    variableTable[1].value = variableTable[2]
+    variableTable[1].allowIncrease = (variableTable[3] == 'inc')
+    variableTable[1].allowDecrease = (variableTable[3] == 'dec')
+end
+
 function Obj.enableMemoryRecords(memoryRecordTable)
     for i = 1, #memoryRecordTable do
-        local mr = memoryRecordTable[i]
+        local tbl = type(memoryRecordTable[i]) == 'table' and memoryRecordTable[i]
+        local mr = tbl and tbl[1] or memoryRecordTable[i]
         if not mr then Obj.log("enableMemoryRecords mr['"..i.."'] not found"); goto continue end
         mr.active = true
         local msg = mr.active and "Enabled" or "Failed"
         Obj.log(msg..": "..mr.description)
+        if mr.active and tbl then enableMemoryRecordVariable(tbl) end
         ::continue::
     end
     return Obj.log("")
@@ -120,23 +131,23 @@ end
 ---------------------------------------------------------------------------------------------------
 local function assignToSectionTable(sectionTable, base)
     for i = 0, sectionTable.count - 1 do
-        local tbl = {}
-        tbl.Name                 = readString(base + i * 0x28, 8)
-        tbl.PhysicalAddress      = readInteger(base + 0x04 + i * 0x28)
-        tbl.VirtualSize          = readInteger(base + 0x08 + i * 0x28)
-        tbl.VirtualAddress       = readInteger(base + 0x0C + i * 0x28)
-        tbl.SizeOfRawData        = readInteger(base + 0x10 + i * 0x28)
-        tbl.PointerToRawData     = readInteger(base + 0x14 + i * 0x28)
-        tbl.PointerToRelocations = readInteger(base + 0x18 + i * 0x28)
-        tbl.PointerToLinenumbers = readInteger(base + 0x1C + i * 0x28)
-        tbl.NumberOfRelocations  = readSmallInteger(base + 0x20 + i * 0x28)
-        tbl.NumberOfLinenumbers  = readSmallInteger(base + 0x22 + i * 0x28)
-        tbl.Characteristics      = readInteger(base + 0x24 + i * 0x28)
-        sectionTable[#sectionTable + 1] = tbl
+        sectionTable[#sectionTable + 1] = {
+            Name                 = readString(base + i * 0x28, 8),
+            PhysicalAddress      = readInteger(base + 0x04 + i * 0x28),
+            VirtualSize          = readInteger(base + 0x08 + i * 0x28),
+            VirtualAddress       = readInteger(base + 0x0C + i * 0x28),
+            SizeOfRawData        = readInteger(base + 0x10 + i * 0x28),
+            PointerToRawData     = readInteger(base + 0x14 + i * 0x28),
+            PointerToRelocations = readInteger(base + 0x18 + i * 0x28),
+            PointerToLinenumbers = readInteger(base + 0x1C + i * 0x28),
+            NumberOfRelocations  = readSmallInteger(base + 0x20 + i * 0x28),
+            NumberOfLinenumbers  = readSmallInteger(base + 0x22 + i * 0x28),
+            Characteristics      = readInteger(base + 0x24 + i * 0x28)
+        }
     end
     return sectionTable
 end
----------------------------------------------------------------------------------------------------
+
 function Obj.createSectionTable(moduleNameOrAddress)
     local sectionTable = {}
     local addr = getAddress(moduleNameOrAddress)
@@ -154,6 +165,7 @@ end
 ---------------------------------------------------------------------------------------------------
 function Obj.getSectionRegion(sectionTable, selectIndex)
     local section = selectIndex and sectionTable[selectIndex] or sectionTable[1]
+    assert(section, "Invalid section")
     local start = sectionTable.imageBase + section.VirtualAddress
     return start, section.VirtualSize
 end
@@ -162,16 +174,16 @@ end
 ---------------------------------------------------------------------------------------------------
 local function findMethodBySignature(className, methodName, signature)
     -- Thanks: https://fearlessrevolution.com/viewtopic.php?p=214984#p214984
+    assert(className and methodName and signature, "Invalid arguments")
     local id  = mono_findClass("", className)
     local tbl = mono_class_enumMethods(id)
     for i = 1, #tbl do
-        local specific = tbl[i]
-        local isName = (specific.name == methodName)
-        local sig = isName and mono_method_getSignature(specific.method) or ""
-        if sig:match(signature) then return specific.method end
+        local isName = (tbl[i].name == methodName)
+        local sig = isName and mono_method_getSignature(tbl[i].method) or ""
+        if sig:match(signature) then return tbl[i].method end
     end
 end
----------------------------------------------------------------------------------------------------
+
 function Obj.getJitRegion(className, methodName, signature)
     local sig = signature and findMethodBySignature(className, methodName, signature)
     local id  = sig or mono_findMethod("", className, methodName)
@@ -187,18 +199,59 @@ function Obj.labelAndSymbol(parameters, syntaxcheck)
     return 'label('..parameters..')\r\nregistersymbol('..parameters..')'
 end
 ---------------------------------------------------------------------------------------------------
-function Obj.loadScript(activeList)
-    if (#activeList == 0) then error(messageDialog('No active scripts.', 2, 2)) end
-    local str = '{$lua}\nif syntaxcheck then return end\n\n[ENABLE]\n\nassert(AddressList.getMemoryRecordByID(10000).active)\n\nlocal mr = {\n'
-    for i = 1, #activeList do
-        local al = ('    AddressList.getMemoryRecordByID(%d)'):format(activeList[i])
-        str = (i == #activeList) and str..al..'\n}\n\n' or str..al..',\n'
+-- loadCheckboxStates functions
+---------------------------------------------------------------------------------------------------
+local function getVariableState(mr)
+    if (mr.value == '') then return end
+    local inc = mr.allowIncrease and 'inc' or nil
+    local dec = mr.allowDecrease and 'dec' or nil
+    return mr.value, inc or dec
+end
+
+local function handleHeader(header, mr, list)
+    if mr.description == '<settings>' or not mr.active then return end
+    list[#list + 1] = {mr, getVariableState(mr)}
+    for i = 0, mr.count - 1 do handleHeader(header, mr[i], list) end
+end
+
+local function getEnableList(header)
+    local list = {}
+    for i = 0, header.count - 1 do handleHeader(header, header[i], list) end
+    return list
+end
+
+local function getStateString(arg)
+    local parent = arg[1].Parent.description
+    local child  = arg[1].description
+    local str = [=[Recorder.getChild(AddressList["%s"], "%s")]=]
+    if not arg[3] then return ('    '..str):format(parent, child) end
+    local mr = str:format(parent, child)
+    local allow = arg[3] and (", '%s'"):format(arg[3]) or ''
+    return ("    {%s, '%s'%s}"):format(mr, arg[2], allow)
+end
+
+function Obj.addEnableList(tbl)
+    local enableList = getEnableList(getAddressList().getMemoryRecordByID(777))
+    for i = 1, #enableList do local mr = enableList[i][1]; tbl[#tbl + 1] = mr end
+    return tbl
+end
+
+function Obj.loadCheckboxStates(mainMemrec, loadMemrec)
+    local enableList = getEnableList(mainMemrec)
+    if (#enableList == 0) then error(messageDialog('No checked boxes were found.', 2, 2)) end
+    local str = '{$lua}\nif syntaxcheck then return end\n\n[ENABLE]\n\nlocal mr = {\n'
+    for i = 1, #enableList do
+        local al = getStateString(enableList[i])
+        str = (i == #enableList) and str..al..'\n}\n\n' or str..al..',\n'
     end
-    return str.."Register.enableMemoryRecords(mr); speakEnglish('Executed'); return 'nop'\n\n[DISABLE]"
+    loadMemrec.script = str.."synchronize(function() Register.enableMemoryRecords(mr) end); return 'nop'\n\n[DISABLE]"
+    local info = translate("You haven't saved your last changes yet. Save Now?")
+    assert(messageDialog(info, 3, 0, 1) == mrYes)
+    mainMemrec.active = false
+    return getMainForm().Save1.doClick()
 end
 ---------------------------------------------------------------------------------------------------
-function Obj.log(...)
-    return Obj.isDebug and printf(...)
-end
+function Obj.log(...) return Obj.isDebug and print(...) end
 ---------------------------------------------------------------------------------------------------
 return Obj
+---------------------------------------------------------------------------------------------------
