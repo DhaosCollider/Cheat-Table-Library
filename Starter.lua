8f5f0486fde1c0d1e03d050bd09a8a2f79151b05
DhaosCollider <72247447+DhaosCollider@users.noreply.github.com>
2025-04-22 10:18:30 +0900

    Update Starter.lua
    Supports CE 7.6

--- Starter.lua
+++ Starter.lua
@@ -4,37 +4,40 @@
 ---------------------------------------------------------------------------------------------------
 local Obj = {}
 ---------------------------------------------------------------------------------------------------
-function Obj.onEnable(MainForm, AddressList, process)
-    local success = readInteger(process) or pcall(synchronize, MainForm.sbOpenProcess.doClick)
-    local startTime = assert(success, 'Error: MainForm.sbOpenProcess.doClick') and os.clock()
-    Obj.log(('The table has %d options.'):format(Obj.getTableOptionCount(AddressList)))
-    AddressList.rebuildDescriptionCache()
+function Obj.onEnable(MainForm, AddressList)
+    local time = os.clock()
+    assert(getAddressSafe('$process'), translate(MainForm.sbOpenProcess.hint))
+    print(('The table has %d options.'):format(Obj.getTableOptionCount(AddressList)))
     local mr, msg = AddressList.getMemoryRecordByID(777), "Activate table settings."
     mr.description = "<< "..msg
-    Obj.log(msg.."\n")
+    print(msg.."\n")
     local setting = AddressList.getMemoryRecordByID(10000)
     setting.active = true
-    local sec = (' (%.2fs)'):format(os.clock() - startTime)
+    local sec = (' (%.2fs)'):format(os.clock() - time)
     local isActive = setting.active and "completed." or "unfinished."
-    Obj.log(setting.description.." Setup "..isActive..sec.."\n")
+    print(setting.description.." Setup "..isActive..sec.."\n")
     mr.description, mr.color = '>> '..setting.description, Obj.enabledColor
+    return Obj.isDebug or getLuaEngine().hide()
 end
 ---------------------------------------------------------------------------------------------------
 function Obj.onDisable(AddressList)
+    local time = os.clock()
     AddressList.rebuildDescriptionCache()
     local al, info = AddressList.getMemoryRecordByID(777), "Initialize table settings."
     al.description = ">> "..info
-    Obj.log(info.."\n"); Obj.removeAdvOptions(); Obj.removeStructures()
+    print(info.."\n"); synchronize(Obj.removeAdvOptions); synchronize(Obj.removeStructures)
     local setting = AddressList.getMemoryRecordByID(10000)
     setting.active = false
     local msg = not setting.active and "completed." or "unfinished."
-    local sec = (' (%.2fs)'):format(al.asyncProcessingTime / 1000)
-    Obj.log("Initialization "..msg..sec.."\n")
+    local sec = (' (%.2fs)'):format(os.clock() - time)
+    print("Initialization "..msg..sec.."\n")
     al.description, al.color = "<< Check the box.", Obj.disabledColor
+    return Obj.isDebug or getLuaEngine().hide()
 end
 ---------------------------------------------------------------------------------------------------
 function Obj.recommandInit(sender)
     -- Thanks: https://forum.cheatengine.org/viewtopic.php?t=602700
+    if not readInteger("$process") then return closeCE() end
     local al = AddressList.getMemoryRecordByID(777)
     if not al then Obj.onClose(sender); return caFree end
     if not al.active then Obj.onClose(sender); return caFree end
@@ -42,6 +45,34 @@ function Obj.recommandInit(sender)
     local ret = messageDialog(msg, 3, 0, 1)
     if not (ret == mrYes) then Obj.onClose(sender); return caFree end
     al.active = false
+end
+---------------------------------------------------------------------------------------------------
+-- showSelectionList functions
+---------------------------------------------------------------------------------------------------
+local function showDropdown(memrec)
+    local desc = ("Selection List : %s"):format(memrec.description)
+    local id, str = showSelectionList(desc, '', memrec.DropdownList)
+    if (-1 < id) then return str:match('^(.+):'), str:match(':(.+)$') end
+end
+
+function Obj.showSelectionList(list, row)
+    if not (32 < row.DropDownCount) then return end
+    local isLinked = not (row.DropDownLinkedMemrec == '')
+    local str = showDropdown(isLinked and list[row.DropDownLinkedMemrec] or row)
+    if str then row.value = str end
+end
+---------------------------------------------------------------------------------------------------
+function Obj.addPointerHeaderDescriptions(list, row)
+    if not row.isAddressGroupHeader then return end
+    if (row.OffsetCount == 0) and string.find(row.description, "P->") then return end
+    row.description = string.match(row.description, "Group %d+") and "P->" or row.description
+end
+---------------------------------------------------------------------------------------------------
+function Obj.changeTAddressListControl()
+    local al = assert(getAddressList(), "changeTAddressListControl interrupted")
+    al.onDescriptionChange = Obj.addPointerHeaderDescriptions
+    al.onValueChange = Obj.showSelectionList
+    al.Control[0].backgroundColor = darkMode() and 0x282020 or 0xE8F0F0
 end
 ---------------------------------------------------------------------------------------------------
 function Obj.getTForm(name)
@@ -88,10 +119,32 @@ function Obj.getCheatTableName(path)
     return path and path:match("\\([^\\]+)$") or 'Cheat Table'
 end
 ---------------------------------------------------------------------------------------------------
-function Obj.onLoad(AddressList, description)
-    if not AddressList[description].script:find('Register') then return end
-    AddressList[description].active = (messageDialog('Load Checkbox States?', 3, 0, 1) == mrYes)
+function Obj.onLoad(loadMemrec)
+    if not loadMemrec.script:find('Register') then return end
+    synchronize(function()
+        loadMemrec.active = (messageDialog('Load Checkbox States?', 3, 0, 1) == mrYes)
+    end)
 end
+---------------------------------------------------------------------------------------------------
+function Obj.lanchBrowser(url, info)
+    assert(url:match("^http") or url:match("^www"), "Invalid URL.")
+    if not (type(info) == "string") then return shellExecute(url) end
+    synchronize(function()
+        return (messageDialog(info, 3, 0, 1) == mrYes) and shellExecute(url)
+    end)
+end
+---------------------------------------------------------------------------------------------------
+Obj.info = [[
+⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⠸⣿⣿⣿⣿⣿⣿⣿⣿⣶⡀⡀⡀⡀⡀⡀⣿⣿⡏⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀
+⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⣿⣿⣿⡀⡀⠙⢿⣿⣿⣆⡀⡀⡀⢸⣿⣿⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⣀⣠⣤⣤⣤⡀⡀⡀⡀⡀⡀⣠⣴⣶⣶⣤⡀⡀⡀⡀⡀⢀⣴⣶⣶⣦⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀
+⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⢰⣿⣿⠃⡀⡀⡀⡀⠹⣿⣿⡀⡀⡀⣿⣿⡟⡀⣠⣶⣶⣶⡆⡀⡀⡀⣠⣿⣿⣿⠿⣿⣿⡇⡀⡀⡀⣴⣿⣿⣿⣿⣿⣿⣧⡀⡀⣠⣿⣿⣿⠟⠿⣿⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀
+⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⣿⣿⣿⡀⡀⡀⡀⡀⡀⣿⣿⡀⡀⢠⣿⣿⣵⣿⣿⠿⣿⣿⡏⡀⡀⣼⣿⣿⠋⡀⢸⣿⣿⡀⡀⡀⣾⣿⣿⠋⡀⡀⣹⣿⣿⡀⡀⣿⣿⣏⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀
+⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⢀⣿⣿⠇⡀⡀⡀⡀⡀⣼⣿⣿⡀⡀⣾⣿⣿⣿⠟⡀⣼⣿⣿⡀⡀⣰⣿⣿⠁⡀⣠⣿⣿⡏⡀⡀⣾⣿⡿⡀⡀⡀⡀⣿⣿⡟⡀⡀⠙⢿⣿⣿⣿⣦⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀
+⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⣼⣿⣿⡀⡀⡀⢀⣴⣿⣿⣿⠁⡀⡀⣿⣿⣿⠃⡀⢀⣿⣿⡇⡀⡀⣿⣿⠇⢀⣾⣿⣿⣿⡀⡀⡀⣿⣿⠁⡀⡀⢀⣾⣿⡿⡀⡀⡀⠐⡀⡀⢙⣿⣿⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀
+⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⣿⣿⣏⣠⣴⣾⣿⣿⣿⠟⡀⡀⡀⣸⣿⣿⠁⡀⡀⣾⣿⣿⡀⡀⡀⢿⣿⣿⣿⣿⣿⣿⣿⡀⡀⡀⣿⣿⣷⣤⣶⣿⣿⠟⡀⡀⡀⣶⣶⣾⣿⣿⣿⠋⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀
+⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⣰⣿⣿⣿⣿⣿⣿⣿⡿⠛⠉⡀⡀⡀⡀⡀⠻⠿⠏⡀⡀⡀⠿⣿⠿⡀⡀⡀⡀⠉⠉⠁⡀⠛⠛⠃⡀⡀⡀⠈⠻⠿⠿⠿⠛⠁⡀⡀⡀⡀⠉⠛⠛⠋⠁⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀
+⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⠙⠛⠛⠛⠋⠉⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀⡀
+]]
 ---------------------------------------------------------------------------------------------------
 function Obj.log(...) return Obj.isDebug and print(...) end
 ---------------------------------------------------------------------------------------------------
